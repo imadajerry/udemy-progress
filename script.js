29dd639a45f158d1bc833bb64f139df9ee088903
Jerry <jerrylalrinmawia@gmail.com>
2025-04-09 15:01:24 +0530

    remove google oauth import

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/11.6.0/firebase-app.js";
 import { getFirestore, doc, getDoc, setDoc } from "https://www.gstatic.com/firebasejs/11.6.0/firebase-firestore.js";
-import { getAuth, onAuthStateChanged, signInWithPopup, GoogleAuthProvider, signOut, signInWithEmailAndPassword, createUserWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/10.8.1/firebase-auth.js";
 
 const firebaseConfig = {
   apiKey: "AIzaSyBydkLqBW6p9Dyy47ScQ70SCcgsJSls15E",
